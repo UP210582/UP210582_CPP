1843c7f1ca326cd4f32957ac0e3afcb7fcaf718d
UP210582 <up210582@alumnos.upa.edu.mx>
2022-09-12 10:49:33 -0500

    Hello World

---
+++ Programacion/U1/01_Hola.cpp
@@ -0,0 +1,26 @@
+/*
+Date: 12/09/2022
+Author: Leonardo Asuencio Martinez Torres
+Email: up210582@alumnos.upa.edu.mx
+Description: My first program in C++ (The typical Hello World)
+Last Modification: 12/09/2022
+*/
+
+//------------ Preprocessor Directives ----------------
+
+// library for input and output between screen and keyboard
+#include <iostream>
+
+// ----------- Main Function -------------
+
+int main()
+{
+	std::cout << "The Typical Hello World !! Written from C++ Hello World !!" ; // Write a message on the screen
+
+	/*
+	 If there are no errors, the program ends its execution.
+	*/
+
+    //Return 0 means that the program ands succesfully
+	return 0;
+}
