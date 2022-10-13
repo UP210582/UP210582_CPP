4ca343a4e7dafc9c57dc327854719c78b49c9423
UP210582 <up210582@alumnos.upa.edu.mx>
2022-10-13 10:39:09 -0500

    b

---
+++ Programacion/U2 Control Structures and Cycles/09_Biseccion.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>//biblioteca para poder utilizar la funcion pow() 
+using namespace std; 
+/*para encontrar una raiz de la ecuación f(x)=0 analiticamente, proporcionar la funcion
+F(X)
+DATOS:
+-->a Y b que forman un intervalo, en donde se halla una raiz
+-->TOL criterio de convergencia//la tolerancia
+-->EX criterio de exactitud
+-->MAXIT número maximo de iteraciones
+RESULTADOS:
+-->X la raiz aproximada o mensaje de falla
+*/
+double F(double x){
+    
+    //3*x^3-2*x-5->función tomado como referencia
+    return 3*pow(x,3)-2*x-5;
+}
+double biseccion(double a,double b,int MAXIT,double TOL,double EX){
+    int cont=1;
+    double c;
+    double fc;
+    while(cont<MAXIT){
+        c=(a+b)/2;
+        fc=F(c);
+        if(abs(b-a)<TOL)
+            return c;
+        if(abs(fc)<EX)
+            return c;
+        if(F(a)*fc<0)
+            b=c;
+        if(fc*F(b)<0)
+            a=c;
+        cont=cont+1;
+    }
+    return c;
+} 
+
+int main (int argc, char *argv[]) {
+    cout<<biseccion(0,2,100,0.0005,0.00005);
+    return 0; 
+}
