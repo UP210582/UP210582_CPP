abbd2d559c56cc4265961b27e2813a1870168bb7
UP210582 <up210582@alumnos.upa.edu.mx>
2022-10-13 10:41:30 -0500

    Biseccion

--- Programacion/U2 Control Structures and Cycles/09_Biseccion.cpp
+++ Programacion/U2 Control Structures and Cycles/09_Biseccion.cpp
@@ -1,42 +1,50 @@
-#include<iostream>
-#include<cmath>//biblioteca para poder utilizar la funcion pow() 
-using namespace std; 
-/*para encontrar una raiz de la ecuación f(x)=0 analiticamente, proporcionar la funcion
-F(X)
-DATOS:
--->a Y b que forman un intervalo, en donde se halla una raiz
--->TOL criterio de convergencia//la tolerancia
--->EX criterio de exactitud
--->MAXIT número maximo de iteraciones
-RESULTADOS:
--->X la raiz aproximada o mensaje de falla
+/*
+Date: 13/10/2022
+Author: Leonardo Ausencio Martinez Torres
+Email: up210582@alumnos.upa.edu.mx
+Description: Math method for getting roots
+Last Modification: 13/10/2022
 */
-double F(double x){
-    
-    //3*x^3-2*x-5->función tomado como referencia
-    return 3*pow(x,3)-2*x-5;
-}
-double biseccion(double a,double b,int MAXIT,double TOL,double EX){
-    int cont=1;
-    double c;
-    double fc;
-    while(cont<MAXIT){
-        c=(a+b)/2;
-        fc=F(c);
-        if(abs(b-a)<TOL)
-            return c;
-        if(abs(fc)<EX)
-            return c;
-        if(F(a)*fc<0)
-            b=c;
-        if(fc*F(b)<0)
-            a=c;
-        cont=cont+1;
-    }
-    return c;
-} 
 
-int main (int argc, char *argv[]) {
-    cout<<biseccion(0,2,100,0.0005,0.00005);
-    return 0; 
+//------------ PREPROCESSOR DIRECTIVES ----------------
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//---------------------ISSUE---------------------------
+
+// Make a program that uses to biseccion method to get a root of a graph
+
+//------------------ EXTRA FUNCTION ---------------------
+
+double bisection(double num){
+    //Equation y=x²+3x+8
+    return pow(num,2)-num-12;
+}
+
+//------------------ MAIN FUNCTION ---------------------
+
+int main(){
+    double a,b,c,ya,yb,yc;
+    cout << "Give me the value of (a)";
+    cin >> a;
+    cout << "Give me the value of (b)";
+    cin >> b;
+    do
+    {
+        ya = bisection(a);
+        yb = bisection(b);
+        if ((yb > 0 && ya < 0) || (ya > 0 && yb < 0))
+        {
+            c = (ya+yb)/2;
+            yc = ya;
+            cout << c;
+        }
+        else{
+            cout << "There's no root between those two numbers";
+            break;
+        }
+        
+    } while (yc<=-0.01 || yc>=0.01);
+    return 0;
 }
