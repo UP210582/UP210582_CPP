86d00c69b1d028cee78a314bb63c99ea84ac3359
UP210582 <up210582@alumnos.upa.edu.mx>
2022-10-17 09:25:31 -0500

    Bis

--- Programacion/U2 Control Structures and Cycles/09_Biseccion.cpp
+++ Programacion/U2 Control Structures and Cycles/09_Biseccion.cpp
@@ -6,45 +6,87 @@ Description: Math method for getting roots
 Last Modification: 13/10/2022
 */
 
-//------------ PREPROCESSOR DIRECTIVES ----------------
+// Library for input and output management.
 #include <iostream>
 #include <cmath>
+
+// Library to be able to print only selected digits
+#include <iomanip>
+
+// Use of namespace to avoid std::
 using namespace std;
-
-//---------------------ISSUE---------------------------
-
-// Make a program that uses to biseccion method to get a root of a graph
-
-//------------------ EXTRA FUNCTION ---------------------
-
-double bisection(double num){
-    //Equation y=x²+3x+8
-    return pow(num,2)-num-12;
+// Function to solve the equation
+float resolverEcuacion(float numero)
+{
+    return (pow(numero, 2) - numero - 12);
 }
 
-//------------------ MAIN FUNCTION ---------------------
-
-int main(){
-    double a,b,c,ya,yb,yc;
-    cout << "Give me the value of (a)";
-    cin >> a;
-    cout << "Give me the value of (b)";
-    cin >> b;
-    do
+// Function to print the line of -
+int imprimirlinea()
+{
+    for (int i = 0; i < 161; i++)
     {
-        ya = bisection(a);
-        yb = bisection(b);
-        if ((yb > 0 && ya < 0) || (ya > 0 && yb < 0))
+        cout << "-";
+    }
+    cout << "\n";
+    return 0;
+}
+
+int main()
+{
+    // Declare the variables.
+    int interador = 1;
+    int solucion = 1;
+    float a, b, c, ya, yb, yc = 1;
+
+    // Ask the user for the range
+    cout << "Enter the value A: ";
+    cin >> a;
+    cout << "Enter the value B: ";
+    cin >> b;
+
+    // Print table header
+    imprimirlinea();
+    cout << "| Interdor \t| a \t\t\t| b \t\t\t| c \t\t\t| y(a)\t\t\t| y(b)\t\t\t| y(c)\t\t\t| \n";
+    imprimirlinea();
+
+    while (yc >= 0.01 || yc <= -0.01)
+    {
+        // Obtain point c and Y(a), Y(b), Y(c)
+        c = (a + b) / 2;
+        ya = resolverEcuacion(a);
+        yb = resolverEcuacion(b);
+        yc = resolverEcuacion(c);
+        interador++;
+        // Print the results in a table
+        cout << "| " << interador << "\t\t| " << fixed << setprecision(8) << a << "\t\t| " << b << "\t\t| " << c << "\t\t| " << ya << "\t\t| " << yb << "\t\t| " << yc << "\t\t| \n";
+        imprimirlinea();
+        // If you select which half to grab to return to get the point c and Y(a), Y(b), Y(c)
+        if ((ya * yc) < 0)
         {
-            c = (ya+yb)/2;
-            yc = ya;
-            cout << c;
+            b = c;
         }
-        else{
-            cout << "There's no root between those two numbers";
+        else
+        {
+            a = c;
+        }
+        // If to prevent infinite cycling
+        if (a == b)
+        {
+            solucion = 0;
             break;
         }
-        
-    } while (yc<=-0.01 || yc>=0.01);
+    }
+
+    // Print the root if there is
+    if (solucion != 0)
+    {
+        cout << "The root is :" << c;
+        cout << "\n";
+    }
+    else
+    {
+        cout << "There is no root in that range. \n";
+    }
     return 0;
 }
