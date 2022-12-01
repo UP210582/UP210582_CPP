ad97e49b7f3983ae9e057002669d6b44a705f759
Paulina <up210374@alumnos.upa.edu.mx>
2022-11-30 18:56:36 -0600

    iswinner

--- Programacion/U4 Files and Arrays/Conect4.cpp
+++ Programacion/U4 Files and Arrays/Conect4.cpp
@@ -19,8 +19,9 @@ using namespace std;
 void doBoard();
 int selectCol();
 int checkBusyRow(int);
-void setCol(int,int);
-
+void setCol(int, int);
+bool checkHorizaontalMoves(int, int);
+bool checkVerticalMoves(int, int);
 bool isWinner(int);
 
 int col, row, turn = 1;
@@ -33,56 +34,57 @@ char gameArea[6][7] = {{'p', 'r', 'u', 'e', 'b', 'a', '1'},
 
 int main()
 {
-    int busyRow=-1;
-    bool winner=false;
+    int busyRow = -1;
+    bool winner = false;
     doBoard();
     selectCol();
     busyRow = checkBusyRow(col);
-    if (busyRow==-1){
+    if (busyRow == -1)
+    {
         cout << "Please, select other coulum: ";
-    }else {
+    }
+    else
+    {
         setCol(row, col);
-        system ("clear");
+        system("clear");
         doBoard();
         cout << endl;
         winner = isWinner(col);
         turn++;
-
     }
 }
 
 void doBoard()
 {
 
-char board[22][43];
-char number[1][7]={'1','2','3','4','5','6','7'};
-int x=0;
-int y=0;
-int z=0;
-int w=0;
+    char board[22][43];
+    char number[1][7] = {'1', '2', '3', '4', '5', '6', '7'};
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    int w = 0;
 
-
-for (int  row = 0; row < 22; row++)
+    for (int row = 0; row < 22; row++)
     {
         for (int col = 0; col < 43; col++)
         {
-            
-            if (row%3==0 && col%6!=0 || row == 21)
+
+            if (row % 3 == 0 && col % 6 != 0 || row == 21)
             {
                 cout << "¯";
             }
-            else if (col%6==0)
+            else if (col % 6 == 0)
             {
                 cout << "|";
             }
-            else if (row !=0)
+            else if (row != 0)
             {
-                if (row == 1 && col%3==0)
+                if (row == 1 && col % 3 == 0)
                 {
                     cout << number[x][y];
                     y++;
                 }
-                else if ((row==4|| row==7 || row==10 || row== 13 || row==16 || row ==19) && col%3==0)
+                else if ((row == 4 || row == 7 || row == 10 || row == 13 || row == 16 || row == 19) && col % 3 == 0)
                 {
                     cout << gameArea[z][w];
                     w++;
@@ -91,16 +93,12 @@ for (int  row = 0; row < 22; row++)
                 {
                     cout << " ";
                 }
-               
             }
-            
-            
         }
         cout << endl;
-        
     }
     z++;
-    w=0;
+    w = 0;
 }
 
 int selectCol()
@@ -137,3 +135,140 @@ void setCol(int row, int col)
     gameArea[row][col] = value;
 }
 
+bool checkHorizontalMoves(int row, int col)
+{
+    bool isWinner = false;
+    int coulum = col, acum = 0;
+     
+    while (setCol != '0')
+    {
+        //right
+        if (gameArea[row][col] == '0')
+        {
+            acum++;
+            col++;
+        }
+        else
+        {
+            coulum = col;
+        }
+    }
+    while (setCol != '0' && isWinner == false)
+    {
+        //left
+        if (gameArea[row][col]=='O'){
+            acum++;
+            col--;
+        }else {
+            coulum=col;
+        }
+    }
+    if (acum==4){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+bool checkVerticalMoves(int row, int col){
+    bool isWinner = false;
+    int coulum = col, acum = 0;
+    while (setCol != 'O')
+    {
+        //up
+        if (gameArea[row][col] == '0')
+        {
+            acum++;
+            row++;
+        }
+        else
+        {
+            coulum = col;
+        }
+    }
+    while (value !='0' && isWinner == false)
+    {
+        //down
+        if (gameArea[row][col]=='O'){
+            acum++;
+            row--;
+        }else {
+            coulum=col;
+        }
+    }
+    if (acum==4){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+bool checkPositiveDiagonallyMoves(int row, int col){
+    bool isWinner = false;
+    int coulum = col, acum = 0;
+    while (setCol != 'O')
+    {
+        //up
+        if (gameArea[row][col] == '0')
+        {
+            acum++;
+            col++;
+            row--;
+        }
+        else
+        {
+            coulum = col;
+        }
+    }
+    while (setCol != '0' && isWinner == false)
+    {
+        //down
+        if (gameArea[row][col]=='O'){
+            acum++;
+            col--;
+            row++;
+        }else {
+            coulum=col;
+        }
+    }
+    if (acum==4){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+bool checkNegativeDiagonallyMoves(int row, int col){
+    bool isWinner = false;
+    int coulum = col, acum = 0;
+    while (setCol != 'O')
+    {
+        //up
+        if (gameArea[row][col] == '0')
+        {
+            acum++;
+            col--;
+            row++;
+        }
+        else
+        {
+            coulum = col;
+        }
+    }
+    while (setCol != '0' && isWinner == false)
+    {
+        //down
+        if (gameArea[row][col]=='O'){
+            acum++;
+            col++;
+            row++;
+        }else {
+            coulum=col;
+        }
+    }
+    if (acum==4){
+        return true;
+    }else {
+        return false;
+    }
+}
